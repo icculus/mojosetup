f0ea29ed8b39cc34cfa767da69526a32d4761ce2
Ryan C. Gordon <icculus@icculus.org>
2006-03-24 03:35:42 +0000

    Added generic "include me everywhere for basic sanity stuff" header.

---
+++ universal.h
@@ -0,0 +1,20 @@
+#ifndef _INCL_UNIVERSAL_H_
+#define _INCL_UNIVERSAL_H_
+
+/* Include this file from everywhere...it provides basic type sanity, etc. */
+
+typedef char int8;
+typedef unsigned char uint8;
+typedef short int16;
+typedef unsigned short uint16;
+typedef int int32;
+typedef unsigned int uint32;
+typedef long long int64;
+typedef unsigned long long uint64;
+
+typedef int boolean;
+
+#endif
+
+/* end of universal.h ... */
+
