b0e20b50d64e19e6ef91d4c4520ec5b69d60b5a5
Ryan C. Gordon <icculus@icculus.org>
2006-11-29 11:30:22 +0000

    More lua.

--- setup.lua
+++ setup.lua
@@ -5,41 +5,14 @@ require("schema");
 --  need a small subset of the language, there's a lot of flexibility
 --  available to you if you need it.   http://www.lua.org/
 --
--- Case is significant! Functions we supply are CamelCased, everything
---  else should be lowercased.
+-- All functionality supplied by the installer is encapsulated in the
+--  "MojoSetup" table, so you can use any other symbol name without
+--  clashes.
 --
--- You can use lua as a turing-complete scripting language right inside
---  the config file...here's an example of localizing your install for
---  english (the default), Spanish, and "l33t speak". We set up a table
---  with the translations, and a function called "translate" that we can
---  call at runtime to produce the right information.
-
-translations = {
-    es = {
-        ["README.txt"] = "README_spanish.txt";
-        ["Required for play"] = "Requerido para el juego";
-        ["blahblahblah"] = "blaho blaho blaho";
-    };
-
-    l33t = {
-        ["README.txt"] = "README_l33t.txt";
-        ["Required for play"] = "r3qu1r3d 4 pl4y";
-        ["blahblahblah"] = "bl4hbl4hbl4h";
-    };
-}
-
-translation = translations[GetLanguage()]
-function translate(origStr)
-    return (translation and translation[origStr]) or origStr
-end
-
-
-
-
 -- So here's the actual configuration...we used loki_setup's xml schema
 --  as a rough guideline.
 
-Install
+MojoSetup.Install
 {
     product = "mygame";
     desc = "My Game";
@@ -56,25 +29,25 @@ Install
     --  may sanitize the values, add defaults, and verify the data.
 
     -- End User License Agreement(s). You can specify multiple files.
-    Eula { keepdirs = true, filename = "MyGame/MyGame_EULA.txt" };
-    Eula { keepdirs = true, filename = "MyGame/PunkBuster_EULA.txt" };
+    MojoSetup.Eula { keepdirs = true, filename = "MyGame/MyGame_EULA.txt" };
+    MojoSetup.Eula { keepdirs = true, filename = "MyGame/PunkBuster_EULA.txt" };
 
     -- README file(s) to show and install. Note the "translate" call.
-    Readme { filename = translate("README.txt") };
+    MojoSetup.Readme { filename = translate("README.txt") };
 
     -- Specify media (discs) we may need for this install and how to find them.
-    Media { id = "cd1", name = "MyGame CD 1", uniquefile = "Sound/blip.wav" };
-    Media { id = "cd2", name = "MyGame CD 2", uniquefile = "Maps/town.map" };
+    MojoSetup.Media { id = "cd1", name = "MyGame CD 1", uniquefile = "Sound/blip.wav" };
+    MojoSetup.Media { id = "cd2", name = "MyGame CD 2", uniquefile = "Maps/town.map" };
 
     -- Specify chunks to install...optional or otherwise.
-    Option
+    MojoSetup.Option
     {
         required = true;
         size = "600M";
         description = "Base Install";
 
         -- File(s) to install.
-        File
+        MojoSetup.File
         {
             cdromid = "cd1";
             dst = "MyGame/MyGame.app";
---
+++ translations.lua
@@ -0,0 +1,17 @@
+MojoSetup.translations = {
+    ["README.txt"] = {
+        es = "README_spanish.txt";
+        l33t = "README_l33t.txt";
+    };
+
+    ["Required for play"] = {
+        es = "Requerido para el juego";
+        l33t = "r3qu1r3d 4 pl4y";
+    };
+
+    ["blahblahblah"] = {
+        es = "blaho blaho blaho";
+        l33t = "bl4hbl4hbl4h";
+    };
+}
+
