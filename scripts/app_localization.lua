d02665b640aeecc95da604fc93d62d032684fe6d
Ryan C. Gordon <icculus@icculus.org>
2008-01-20 02:11:21 +0000

    Forgot to add this.

---
+++ scripts/app_localization.lua
@@ -0,0 +1,11 @@
+-- Lines starting with "--" are comments in this file.
+-- You should add your installer's strings here, instead of localization.lua,
+--  but localization.lua has instructions on how this file is formatted.
+
+MojoSetup.applocalization = {
+};
+
+-- end of app_localization.lua ...
+
+
+
