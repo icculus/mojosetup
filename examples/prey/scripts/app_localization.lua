0fec1b868bb0c5a8c47f63e302a0c8cf6ad32cd6
Ryan C. Gordon <icculus@icculus.org>
2008-11-30 02:20:43 -0500

    Adding retail prey installer example.

---
+++ examples/prey/scripts/app_localization.lua
@@ -0,0 +1,152 @@
+-- Lines starting with "--" are comments in this file.
+-- You should add your installer's strings here, instead of localization.lua,
+--  but localization.lua has instructions on how this file is formatted.
+
+MojoSetup.applocalization = {
+    -- We're overriding a string in the installer core, so please translate
+    -- the en version, not "Uninstall complete."
+    ["Uninstall complete"] = {
+        en = "Uninstall complete. You may also delete '.prey' in your home directory to remove personal savegames and settings.",
+        de = "Deinstallation komplett. Du kannst auch '.prey' in deinem Heimverzeichnis löschen, um eigene Spielstände und Einstellungen zu entfernen.",
+        es = "Desinstalación completa. Puedes borrar '.prey' en tu directorio home para borrar configuraciones personales y tus juegos grabados.",
+    };
+
+    -- Change this to a unique filename if you translate the file.
+    ["prey_readme.txt"] = {
+        de = "prey_readme_de.txt",
+        es = "prey_readme_es.txt",
+    };
+
+    -- don't change this; we only have four translations of this text file.
+    ["prey_license.txt"] = {
+        fr = "prey_license_fr.txt",
+        it = "prey_license_it.txt",
+        de = "prey_license_de.txt",
+        es = "prey_license_es.txt",
+    };
+
+    -- don't change this; we only have four translations of this text file.
+    ["punkbuster_license.txt"] = {
+        fr = "punkbuster_license_fr.txt",
+        it = "punkbuster_license_it.txt",
+        de = "punkbuster_license_de.txt",
+        es = "punkbuster_license_es.txt",
+    };
+
+    ["Initial language and keyboard layout"] = {
+        de = "Vorgegebene Sprache und Tastaturbelegung.",
+        es = "Configuración inicial de idioma y teclado",
+    };
+
+    ["First-person shooter from Human Head and 3D Realms"] = {
+        de = "Ego-Shooter von Human Head und 3D Realms",
+        es = "Juego de acción en primera persona de Human Head y 3D Realms",
+    };
+
+    ["Prey README"] = {
+        de = "Prey-README",
+        es = "LEEME",
+    };
+
+    ["Prey License"] = {
+        en_GB = "Prey Licence",
+        en_CA = "Prey Licence",
+        de = "Prey-Lizenz",
+        es = "Licencia de uso de Prey",
+    };
+
+    ["PunkBuster(TM) support"] = {
+        de = "PunkBuster(TM)-Unterstützung",
+        es = "Soporte para PunkBuster(R)",
+    };
+
+    ["PunkBuster(TM) License"] = {
+        en_GB = "PunkBuster(TM) Licence",
+        en_CA = "PunkBuster(TM) Licence",
+        de = "PunkBuster(TM)-Lizenz",
+        es = "Licencia de uso de PunkBuster(R)",
+    };
+
+    ["Prey Disc 1"] = {
+        de = "Prey Disc 1",  -- apparently this is best for CD _and_ DVD.
+    };
+
+    ["Prey Disc 2"] = {
+        de = "Prey Disc 2",  -- apparently this is best for CD _and_ DVD.
+    };
+
+    ["Prey Disc 3"] = {
+        de = "Prey Disc 3",  -- apparently this is best for CD _and_ DVD.
+    };
+
+    ["English"] = {
+        zh_TW = "英語",
+        nl = "Engels",
+        fr = "Anglais",
+        de = "Englisch",
+        el = "Αγγλικα",
+        it = "Inglese",
+        ja = "英語",
+        pt = "Inglês",
+        ru = "Английский",
+        es = "Inglés",
+    };
+
+    ["French"] = {
+        zh_TW = "法語",
+        nl = "Frans",
+        fr = "Français",
+        de = "Französisch",
+        el = "Γαλλικα",
+        it = "Francese",
+        ja = "フランス語",
+        pt = "Francês",
+        ru = "Французский",
+        es = "Francés",
+    };
+
+    ["German"] = {
+        zh_TW = "德語",
+        nl = "Duits",
+        fr = "Allemand",
+        de = "Deutsch",
+        el = "Γερμανικα",
+        it = "Tedesco",
+        ja = "ドイツ語",
+        pt = "Alemão",
+        ru = "Немецкий",
+        es = "Alemán",
+    };
+
+    ["Italian"] = {
+        zh_TW = "意大利語",
+        nl = "Italiaans",
+        fr = "Italien",
+        de = "Italienisch",
+        el = "Ιταλικα",
+        it = "Italiano",
+        ja = "イタリア語",
+        pt = "Italiano",
+        ru = "Итальянский",
+        es = "Italiano",
+    };
+
+    ["Spanish"] = {
+        zh_TW = "西班牙語",
+        nl = "Spaans",
+        fr = "Espagnol",
+        de = "Spanisch",
+        el = "Ισπανικα",
+        it = "Spagnolo",
+        ja = "スペイン語",
+        pt = "Espanhol",
+        ru = "Испанский",
+        es = "Español",
+    };
+};
+
+-- end of app_localization.lua ...
+
+
+
+
---
+++ examples/prey/scripts/config.lua
@@ -0,0 +1,247 @@
+local GAME_INSTALL_SIZE = 1738268387;
+local PB_INSTALL_SIZE = 3738916;
+local ENCFG_INSTALL_SIZE = 1689;
+local FRCFG_INSTALL_SIZE = 1739;
+local ESCFG_INSTALL_SIZE = 1731;
+local DECFG_INSTALL_SIZE = 1730;
+local ITCFG_INSTALL_SIZE = 1731;
+
+local _ = MojoSetup.translate
+
+-- change where we look for the "disc" if we are installing from a copy
+--  downloaded through Steam or otherwise installed on Windows:
+-- ./prey-installer.bin --from-install --media "/mnt/ntfsdisk/Program Files/Steam/common/prey/base"
+local media_path = "Setup/Data/Base"
+if MojoSetup.cmdline("from-install") then
+    media_path = ""
+end
+
+-- do a filter to catch filename case differences.
+-- Please note this returns a filter function, and isn't the filter itself!
+local function make_pakfile_filter(pakregexp)
+    -- Upvalue!
+    local regexp = "^" .. string.lower(media_path) .. "/(" .. pakregexp .. ")$"
+    return function(dest)
+        local str, matches
+        str, matches = string.gsub(string.lower(dest), regexp, "base/%1", 1)
+        if matches == 1 then
+            return str, nil
+        end
+        return nil, nil  -- don't install anything else here.
+    end
+end
+
+Setup.Package
+{
+    vendor = "icculus.org",
+    id = "prey",
+    description = "Prey";
+    version = "1.4",
+    splash = "splash.bmp",
+    superuser = false,
+    write_manifest = true,
+    support_uninstall = true,
+    recommended_destinations =
+    {
+        MojoSetup.info.homedir,
+        "/opt/games",
+        "/usr/local/games"
+    },
+
+    -- disc 1, 2, and 3 might all be the same disc for the DVD version!
+    Setup.Media
+    {
+        id = "disc1",
+        description = _("Prey Disc 1"),
+        uniquefile = media_path .. "/pak000.pk4"
+    },
+
+    Setup.Media
+    {
+        id = "disc2",
+        description = _("Prey Disc 2"),
+        uniquefile = media_path .. "/pak002.pk4"
+    },
+
+    Setup.Media
+    {
+        id = "disc3",
+        description = _("Prey Disc 3"),
+        uniquefile = media_path .. "/pak003.pk4"
+    },
+
+    Setup.Readme
+    {
+        description = _("Prey README"),
+        source = _("prey_readme.txt")
+    },
+
+    Setup.Eula
+    {
+        description = _("Prey License"),
+        source = _("prey_license.txt")
+    },
+
+    Setup.Option
+    {
+        value = true,
+        required = true,
+        disabled = false,
+        bytes = GAME_INSTALL_SIZE,
+        description = "Prey",
+
+        Setup.File
+        {
+            source = "media://disc1/",
+            filter = make_pakfile_filter("pak00[01].pk4"),
+        },
+
+        Setup.File
+        {
+            source = "media://disc2/",
+            filter = make_pakfile_filter("pak002.pk4"),
+        },
+
+        Setup.File
+        {
+            source = "media://disc3/",
+            filter = make_pakfile_filter("pak00[34].pk4"),
+        },
+
+
+        -- !!! FIXME: All this filter nonsense is because
+        -- !!! FIXME:   source = "base:///some_dir_in_basepath/"
+        -- !!! FIXME: doesn't work, since it wants a file when drilling
+        -- !!! FIXME: for the final archive, not a directory. Fixing this
+        -- !!! FIXME: properly is a little awkward, though.
+
+        Setup.File
+        {
+            wildcards = "prey-linux-data/*";
+            filter = function(fn)
+                fn = string.gsub(fn, "^prey%-linux%-data/", "", 1);
+                return fn, nil;
+            end
+        },
+
+        Setup.File
+        {
+            wildcards = "prey-linux-x86/*";
+            filter = function(fn)
+                fn = string.gsub(fn, "^prey%-linux%-x86/", "", 1);
+                return fn, nil;
+            end
+        },
+
+        Setup.DesktopMenuItem
+        {
+            disabled = false,
+            name = "Prey",
+            genericname = "Prey",
+            tooltip = _("First-person shooter from Human Head and 3D Realms"),
+            builtin_icon = false,
+            icon = "prey.png",
+            commandline = "%0/prey",
+            category = "Game",
+        },
+
+        Setup.Option
+        {
+            -- let command line toggle this to enabled by default for automation.
+            value = MojoSetup.cmdline("punkbuster"),
+            required = false,
+            disabled = false,
+            bytes = PB_INSTALL_SIZE,
+            description = _("PunkBuster(TM) support"),
+
+            Setup.Eula
+            {
+                description = _("PunkBuster(TM) License"),
+                source = _("punkbuster_license.txt")
+            },
+
+            Setup.File
+            {
+                wildcards = "punkbuster-linux-x86/*";
+                filter = function(fn)
+                    fn = string.gsub(fn, "^punkbuster%-linux%-x86/", "", 1);
+                    return fn, nil;
+                end
+            },
+        },
+
+        Setup.OptionGroup
+        {
+            disabled = false,
+            description = _("Initial language and keyboard layout"),
+            Setup.Option
+            {
+                value = string.match(MojoSetup.info.locale, "^en") ~= nil,
+                required = false,
+                disabled = false,
+                bytes = ENCFG_INSTALL_SIZE,
+                description = _("English"),
+                Setup.File
+                {
+                    wildcards = "configs/english.cfg";
+                    filter = function(fn) return "base/default.cfg"; end
+                },
+            },
+            Setup.Option
+            {
+                value = string.match(MojoSetup.info.locale, "^fr") ~= nil,
+                required = false,
+                disabled = false,
+                bytes = FRCFG_INSTALL_SIZE,
+                description = _("French"),
+                Setup.File
+                {
+                    wildcards = "configs/french.cfg";
+                    filter = function(fn) return "base/default.cfg"; end
+                },
+            },
+            Setup.Option
+            {
+                value = string.match(MojoSetup.info.locale, "^it") ~= nil,
+                required = false,
+                disabled = false,
+                bytes = ITCFG_INSTALL_SIZE,
+                description = _("Italian"),
+                Setup.File
+                {
+                    wildcards = "configs/italian.cfg";
+                    filter = function(fn) return "base/default.cfg"; end
+                },
+            },
+            Setup.Option
+            {
+                value = string.match(MojoSetup.info.locale, "^de") ~= nil,
+                required = false,
+                disabled = false,
+                bytes = DECFG_INSTALL_SIZE,
+                description = _("German"),
+                Setup.File
+                {
+                    wildcards = "configs/german.cfg";
+                    filter = function(fn) return "base/default.cfg"; end
+                },
+            },
+            Setup.Option
+            {
+                value = string.match(MojoSetup.info.locale, "^es") ~= nil,
+                required = false,
+                disabled = false,
+                bytes = ESCFG_INSTALL_SIZE,
+                description = _("Spanish"),
+                Setup.File
+                {
+                    wildcards = "configs/spanish.cfg";
+                    filter = function(fn) return "base/default.cfg"; end
+                },
+            },
+        },
+    },
+}
+
+-- end of config.lua ...
+
