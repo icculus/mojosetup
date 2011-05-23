622047ed42401ac75d31bda2c43352218f16d18d
Francois Gouget <fgouget@codeweavers.com>
2011-05-23 23:32:09 +0200

    Fix an infinite loop bug in zip_expand_symlink_path().
    Without this patch it would remain stuck on the first '/' it found in
    the symbolic link target!

--- archive_zip.c
+++ archive_zip.c
@@ -797,6 +797,7 @@ static void zip_expand_symlink_path(char *path)
         else
         {
             prevptr = ptr;
+            ptr++;
         } /* else */
     } /* while */
 } /* zip_expand_symlink_path */
