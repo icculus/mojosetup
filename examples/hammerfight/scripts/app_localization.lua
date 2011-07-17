894ca6bc410f126a59ed6159d1f6c19a90e984bf
Ryan C. Gordon <icculus@icculus.org>
2011-07-17 19:30:38 -0400

    Added Hammerfight example installer. ( http://www.koshutin.com/ )

---
+++ examples/hammerfight/scripts/app_localization.lua
@@ -0,0 +1,23 @@
+-- Lines starting with "--" are comments in this file.
+-- You should add your installer's strings here, instead of localization.lua,
+--  but localization.lua has instructions on how this file is formatted.
+
+MojoSetup.applocalization = {
+    ["Uninstall complete"] = {
+        en = "Uninstall complete. You may also delete 'Hammerfight' in your home directory to remove personal savegames and settings.",
+        de = "Deinstallation komplett. Du kannst auch 'Hammerfight' in deinem Heimverzeichnis löschen, um eigene Spielstände und Einstellungen zu entfernen.",
+        es = "Desinstalación completa. Puedes borrar 'Hammerfight' en tu directorio home para borrar configuraciones personales y tus juegos grabados.",
+        pl = "Usuwanie zakończone. By usunąć zapisane stany gry i ustawienia, można również usunąć 'Hammerfight' z domowego katalogu.",
+        it = "Disinstallazione completata. Se desideri cancellare i salvataggi e le impostazione cancella 'Hammerfight' nella tua directory home.",
+    };
+
+    ["Hammerfight README"] = {
+        de = "Hammerfight-README",
+        es = "LEEME",
+        pl = "Hammerfight README",
+        it = "Informazioni su Hammerfight",
+    };
+};
+
+-- end of app_localization.lua ...
+
---
+++ examples/hammerfight/scripts/config.lua
@@ -0,0 +1,63 @@
+local TOTAL_INSTALL_SIZE = 122128125;
+
+local _ = MojoSetup.translate
+
+Setup.Package
+{
+    vendor = "kranx.com",
+    id = "hammerfight",
+    description = "Hammerfight",
+    version = "1.0",
+    splash = "splash.bmp",
+    superuser = false,
+    write_manifest = true,
+    support_uninstall = true,
+    recommended_destinations =
+    {
+        MojoSetup.info.homedir,
+        "/opt/games",
+        "/usr/local/games"
+    },
+
+    Setup.Readme
+    {
+        description = _("Hammerfight README"),
+        source = _("README-linux.txt")
+    },
+
+    Setup.Option
+    {
+        -- !!! FIXME: All this filter nonsense is because
+        -- !!! FIXME:   source = "base:///some_dir_in_basepath/"
+        -- !!! FIXME: doesn't work, since it wants a file when drilling
+        -- !!! FIXME: for the final archive, not a directory. Fixing this
+        -- !!! FIXME: properly is a little awkward, though.
+
+        value = true,
+        required = true,
+        disabled = false,
+        bytes = TOTAL_INSTALL_SIZE,
+        description = "Hammerfight",
+
+        Setup.File
+        {
+            wildcards = "*";
+        },
+
+        Setup.DesktopMenuItem
+        {
+            disabled = false,
+            name = "Hammerfight",
+            genericname = "Hammerfight",
+            tooltip = _("A game of flying, battling machines"),
+            builtin_icon = false,
+            icon = "hammerfight.png",
+            commandline = "%0/Hammerfight",
+            workingdir = "%0",
+            category = "Game"
+        }
+    }
+}
+
+-- end of config.lua ...
+
