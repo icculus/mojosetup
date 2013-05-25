96cef20f557c16597b85ee6894e7911933c77dfa
Steffen Pankratz <kratz00@gmx.de>
2013-05-25 10:03:12 +0200

    Enhanced filter function (added ent argument)

--- scripts/mojosetup_mainline.lua
+++ scripts/mojosetup_mainline.lua
@@ -588,7 +588,7 @@ local function install_archive_entry(archive, ent, file, option)
 
     if file.filter ~= nil then
         local filterperms
-        dest, filterperms = file.filter(dest)
+        dest, filterperms = file.filter(dest, ent)
         if filterperms ~= nil then
             perms = filterperms
         end
